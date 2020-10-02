0915333915661a850eff6f24b0e7869308d44d7a
Alan Castañeda Ascencio <alan.castaneda5829@alumnos.udg.mx>
2020-10-02 13:59:30 -0500

    actividad7 ver.1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+int main(){
 
-    return 0;
+    string dato;
+    getline(cin,dato);
+
+    if(dato=="char"){
+        cout << sizeof(char) << endl;
+    }else if(dato=="int"){
+        cout << sizeof(int) << endl;
+    }else if(dato=="short"){
+        cout << sizeof(short) << endl;
+    }else if(dato=="float"){
+        cout << sizeof(float) << endl;
+    }else if(dato=="double"){
+        cout << sizeof(double) << endl;
+    }else if(dato=="long"){
+        cout << sizeof(long) << endl;
+    }
+
 }
